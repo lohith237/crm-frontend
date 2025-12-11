d3c21d7ebea02e91fe991747327d4502bf20b609
sairam kandru <lohithsairam10@gmail.com>
2025-12-11 20:57:04 +0530

    adeed base url

--- services/axiosConfig.js
+++ services/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://192.168.1.21:3000",
+    baseURL:"https://mern-mini-crm-backend-xgol.onrender.com",
 });
 
 api.interceptors.request.use(
